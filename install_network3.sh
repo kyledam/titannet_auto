4b50791957ec178f6122940ad74e01f1ad68f9cc
kyledam <khoi.dam027@gmail.com>
2024-05-08 18:15:12 +0700

    Update install_network3.sh

--- install_network3.sh
+++ install_network3.sh
@@ -10,26 +10,4 @@ cd ubuntu-node || exit
 # Run the manager.sh up command
 bash manager.sh up
 
-# Create the service file content
-service_content=$(cat <<-END
-[Unit]
-Description=Titan Node Manager
-After=network.target
 
-[Service]
-ExecStart=/bin/bash /root/ubuntu-node/manager.sh up
-Restart=always
-RestartSec=10
-
-[Install]
-WantedBy=multi-user.target
-END
-)
-
-# Create the service file
-echo "$service_content" | sudo tee /etc/systemd/system/network3.service > /dev/null
-
-# Reload systemd daemon and start the service
-sudo systemctl daemon-reload
-sudo systemctl enable network3.service
-sudo systemctl start network3.service
