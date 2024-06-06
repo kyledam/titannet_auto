2a357b0e207469650fa36ad6d605ac9a2dff9f25
kyledam <khoi.dam027@gmail.com>
2024-06-06 09:53:49 +0700

    Update autologin.sh

--- autologin.sh
+++ autologin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 rm -f /etc/systemd/system/container-getty@.service.d/override.conf
+mkdir /etc/systemd/system/container-getty@.service.d
 wget https://raw.githubusercontent.com/kyledam/titannet_auto/main/override.conf
 cp override.conf /etc/systemd/system/container-getty@.service.d/override.conf
 rm -f autologin.sh
